ad4b4eca8e9a21850dfa290a2fa5e2b6bbc3022b
haitam-hamdan-veeva <haitam.hamdan@veeva.com>
2024-10-16 19:56:44 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         CDMS Vault Redirect
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-16
+// @description  Shortcut to access CDMS redirect Vaults
+// @author       Haitam Hamdan
+// @match        https://*.veevavault.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=veevavault.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const SWITCH_VAULT_PATH = 'veevaHome/switchVault/';
+  const VMC_REDIRECT_URL = 'https://vmc.veevavault.com/vmc/vault/find/';
+
+  const interceptXHRRequests = () => {
+    const originalOpen = XMLHttpRequest.prototype.open;
+
+    XMLHttpRequest.prototype.open = function (method, url) {
+      const requestURL = new URL(url, window.location.origin).href;
+
+      if (requestURL.includes(SWITCH_VAULT_PATH)) {
+        const redirectVaultId = requestURL.split('/').pop();
+
+        alert(
+          `[CDMS Vault Redirect]: Opening target Vault ${redirectVaultId}. Click OK to continue.`
+        );
+
+        window.location.replace(`${VMC_REDIRECT_URL}${redirectVaultId}`);
+      }
+
+      return originalOpen.apply(this, arguments);
+    };
+  };
+
+  interceptXHRRequests();
+})();
